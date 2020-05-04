26f1b8df61b1626635fb25095815cf95d162b715
Rodrigo Hinojosa Acuña <full.stack@rodrigo-hinojosa.dev>
2020-05-03 20:54:16 -0400

    :ok_hand:

--- src/app/utils/index.ts
+++ src/app/utils/index.ts
@@ -3,9 +3,16 @@ import * as path from 'path';
 
 export class Utils {
 
+    /**
+	 * Constructor
+	 */
     constructor() {
     }
 
+    /**
+	 * Small description...
+	 * @returns {string}
+	 */
     static getDateTime(): string {
         const i = new Date();
         return ('00' + (i.getMonth() + 1)).slice(-2) + '-' +
@@ -16,6 +23,10 @@ export class Utils {
             ('00' + i.getSeconds()).slice(-2);
     }
 
+    /**
+	 * Small description...
+	 * @returns {string}
+	 */
     static getTimeStamp(): string {
         const d = new Date();
         return ('00' + (d.getMonth() + 1)).slice(-2) +
@@ -25,10 +36,23 @@ export class Utils {
             ('00' + d.getSeconds()).slice(-2);
     }
 
+    /**
+	 * Small description...
+	 * @param {any} data
+     * @param {string} dir
+     * @param {string} fileName
+     * @param {string} type
+	 * @returns {void}
+	 */
     static writeFileByType(data: any, dir: string, fileName: string, type: string): void {
         fs.writeFileSync(`${dir.concat(fileName)}.${type}`, data, 'utf-8');
     }
 
+    /**
+	 * Small description...
+	 * @param {strng} downloadFolder
+	 * @returns {string}
+	 */
     static createDownloadDirectory(downloadFolder: string): string {
         const downloadDirectory = path.join(__dirname, downloadFolder);
         if (!fs.existsSync(downloadDirectory)) {
